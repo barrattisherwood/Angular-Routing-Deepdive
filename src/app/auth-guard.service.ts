5973291d69d944b9f505fb37be1352c5e189b22f
BIsherwood <barratti@osiristrading.co.za>
2019-01-02 13:28:20 +0200

    Lecture 140 completed

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,9 +1,28 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateChild } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
-// export class AuthGuard implements CanActivate {
-    // canActivate(route: ActivatedRouteSnapshot,
-    //             state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+    constructor(private authService: AuthService, private router: Router) {}
 
-    //             }
-// }
+    canActivate(route: ActivatedRouteSnapshot,
+                state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+                    return this.authService.isAuthenticated()
+                        .then(
+                            (authenticated: boolean) => {
+                                if (authenticated) {
+                                    return true;
+                                } else {
+                                    this.router.navigate(['/']);
+                                }
+                            }
+                        );
+                }
+
+    canActivateChild(route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+            return this.canActivate(route, state);
+        }
+}
